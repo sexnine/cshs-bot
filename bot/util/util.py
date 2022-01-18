a3bcaac30c338a46873bf47933ec226cfd723e0a
sexnine <planz300@gmail.com>
2022-01-19 03:55:36 +1100

    Add vacefron api, add env example, add embed_msg util

--- bot/util/util.py
+++ bot/util/util.py
@@ -1,5 +1,21 @@
 from uuid import uuid4
+from enum import Enum
+import discord
+import vacefron
+
+vac = vacefron.Client()
+
+
+class MsgStatus(Enum):
+    NEUTRAL = None
+    SUCCESS = 0x42ff52
+    WARN = 0xffc942
+    ERROR = 0xff4f42
 
 
 def random_id() -> str:
     return uuid4().hex
+
+
+def embed_msg(text: str, status: MsgStatus = MsgStatus.SUCCESS) -> discord.Embed:
+    return discord.Embed(description=text, color=status.value)
