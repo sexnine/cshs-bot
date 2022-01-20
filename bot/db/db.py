208b4dcc08b495ceaa776801d781b6508561d70e
sexnine <planz300@gmail.com>
2022-01-20 21:32:46 +1100

    Convert User to beanie model, init beanie

--- bot/db/db.py
+++ bot/db/db.py
@@ -1,23 +1,20 @@
 from __future__ import annotations
+import pymongo
 from motor.motor_asyncio import AsyncIOMotorClient
-from odmantic import Field, Model
-from odmantic import AIOEngine
+# from odmantic import Field, Model
+# from odmantic import AIOEngine
+from beanie import Document, Indexed, init_beanie
 from os import getenv
 from typing import Union
 import discord
 from bot.util.config import get_config
 
-db = AIOEngine(AsyncIOMotorClient(getenv("MONGO_URI")), getenv("DB_NAME", "bot"))
+db = AsyncIOMotorClient(getenv("MONGO_URI"))
 
 
-class Saveable:
-    async def save(self):
-        return await db.save(self)
-
-
-class User(Model, Saveable):
-    id: int = Field(primary_field=True)
-    xp: int = 0
+class User(Document):
+    id: int
+    xp: Indexed(int, index_type=pymongo.DESCENDING) = 0
     level: int = 1  # updated whenever "xp > next_level_xp" or xp is manually set
     next_level_xp: int = get_config("levels").get("xp_per_level")  # reduces load on the bot
     previous_level_xp: int = 0  # reduces load on the bot
@@ -25,8 +22,13 @@ class User(Model, Saveable):
     @classmethod
     async def get(cls, user: Union[int, discord.Member, discord.User]) -> User:
         user_id = user if type(user) is int else user.id
-        user = await db.find_one(User, User.id == user_id)
+        user = await cls.find_one(User.id == user_id)
         if not user:
             user = User(id=user_id)
-            await db.save(user)
+            await user.save()
         return user
+
+
+async def init():
+    await init_beanie(database=db[getenv("DB_NAME", "bot")], document_models=[User])
+    print("db initialized")
