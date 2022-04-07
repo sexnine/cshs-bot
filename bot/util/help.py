c443c61f2ba7b4cdbe093ede889bf70ccc1a62e6
sexnine <25860388+sexnine@users.noreply.github.com>
2022-04-07 16:06:52 +1000

    Formatting, Fix code

--- bot/util/help.py
+++ bot/util/help.py
@@ -1,5 +1,6 @@
-from discord import commands
+from discord.ext import commands
 import discord
+
 
 class HelpCommand(commands.MinimalHelpCommand):
     async def send_pages(self):
@@ -7,4 +8,4 @@ class HelpCommand(commands.MinimalHelpCommand):
         embed = discord.Embed(color=discord.Color.yellow(), description='')
         for page in self.paginator.pages:
             embed.description += page
-        await destination.send(embed=e)
+        await destination.send(embed=embed)
