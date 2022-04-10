a5c15adc657cfb9a7238503ef8b552ef75c93e1b
SomeHybrid <91648368+SomeHybrid@users.noreply.github.com>
2022-04-10 18:39:23 +0700

    Fixed bugs in logging. Ready for implementation.

--- bot/util/logger.py
+++ bot/util/logger.py
@@ -1,38 +1,22 @@
 # importing modules
-from os import getcwd, listdir, remove
-from os.path import splitext
+from os import getcwd
 from time import time
 import logging
 
 # config the logging module
-path = getcwd() + "/current.log"
-logging.basicConfig(filename=path, level=logging.INFO, format='%(asctime)s - [%(levelname)s]: %(message)s')
+path = getcwd() + "/logs"
+logging.basicConfig(filename=path + "current.log", level=logging.INFO, format='%(asctime)s - [%(levelname)s]: %(message)s')
 
 # create a new log and flush current.log
-with open("current.log", "r+") as f:
+with open(path + "current.log", "r") as f:
     data = f.read()
-    if data != "":
-        ts = f.readline(0)
-        f.write("")
-        with open(f"{ts}.log", "w") as file:
-            file.write(time() + "\n" + data)
-
-# removes oldest log file if there are more than 10 log files
-files = listdir()
-logs = []
-lowestlog = False
-if len(files) > 11:
-    # checks for the oldest log file
-    for file in files:
-        text = splitext(file)
-        if text[1] == '.log':
-            logs.append(int(text[0]))
-    for log in logs:
-        if not lowestlog or log > lowestlog:
-            lowestlog = log
-    
-    # deletes the log file
-    remove(str(lowestlog) + ".log")
+    with open(path + "current.log", "w") as f1:
+        if data != "":
+            ts = data.split("\n")[0]
+            print(ts)
+            with open(path + f"{ts}.log", "w") as file:
+                file.write(data)
+        f1.write(str(int(time())) + "\n")
 
 
 # implementing the logger class
@@ -47,17 +31,17 @@ class Logger:
     
     # create the logging functions
     def info(self, message):
-        logging.info(f"Logged in cog {self.s.__qualname__}: {message}")
+        logging.info(f"Logged in cog {self.s.__class__.__name__}: {message}")
 
     def debug(self, message):
-        logging.debug(f"Logged in cog {self.s.__qualname__}: {message}")
+        logging.debug(f"Logged in cog {self.s.__class__.__name__}: {message}")
 
     def warning(self, message):
-        logging.warning(f"Logged in cog {self.s.__qualname__}: {message}")
+        logging.warning(f"Logged in cog {self.s.__class__.__name__}: {message}")
 
-    def error(self, message, ctx):
-        logging.error(f"An error occurred in cog {self.s.__qualname__} in command {ctx.command}: {message}")
+    def error(self, message, name):
+        logging.error(f"An error occurred in cog {self.s.__class__.__name__} in command {name}: {message}")
     
     # detect when logger has been uninitialized
     def __del__(self):
-        logging.debug(f"Cog {self.s.__qualname__} has been uninitialized.")
+        logging.debug(f"Cog {self.s.__class__.__name__} has been uninitialized.")
